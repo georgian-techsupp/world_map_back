bb1a6062896dd16143893dae09b95e78a3f17008
Ubuntu <ubuntu@ip-172-31-18-50.eu-central-1.compute.internal>
2024-06-13 16:18:30 +0000

    test

--- world_map/settings.py
+++ world_map/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&jj-*)&-k#ikghpw=mj#xc=9t7lfdq(o3vql83loysc5auof0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api.1inone.com" , "localhost" , "18.192.240.161"]
 
 
 # Application definition
